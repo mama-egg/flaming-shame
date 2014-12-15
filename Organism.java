78c56ce8dc7b17df6d452986750b984165c6c2ce
mama-egg <Kinseygoesrawr@gmail.com>
2014-12-14 20:59:53 -0700

    Update Organism.java

--- Organism.java
+++ Organism.java
@@ -1,9 +1,46 @@
 //Kinsey Clyne 
 //9N
 public class Organism {
-
-private boolean isAlive;
-private int size;
-private int number;
-
-private boolean die() {
+  private boolean isAlive;
+  private int size;
+  private String species;
+  
+  //Get Methods
+  private boolean getLifeStatus() {
+    return isAlive;
+  }
+  
+  private int getSize() {
+    return size;
+  }
+  
+  private String getSpecies() {
+    return species;
+  }
+  
+  private String toString() {
+    return "species: " + species + ", is alive: " + isAlive + ", size: " + size;
+  }
+  
+  //Set Methods
+  private void setLifeStatus(boolean newLifeStatus) {
+    isAlive = newLifeStatus;
+  }
+  
+  private void setSize(int newSize) {
+    size = newSize;
+  }
+  
+  private void setSpecies(String newSpecies) {
+    species = newSpecies;
+  }
+  
+  //Other Mutators
+  private void grow() {
+    size++;
+  }
+  
+  private void die() {
+    isAlive = false;
+  }
+}
