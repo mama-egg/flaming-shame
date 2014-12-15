2704faad8d232b43c07f28d4faba7841ffc2aa50
mama-egg <Kinseygoesrawr@gmail.com>
2014-12-14 21:47:53 -0700

    Update Human.java

--- Human.java
+++ Human.java
@@ -1,3 +1,36 @@
 //Kinsey Clyne
 //9N
 public class Human inherits Animal {
+  
+  super(isAlive, size, species, color, energy, isHungry);
+  
+  public Human() {
+    System.out.println(/t"Human One: ");
+    Human.toString();
+    
+    Human.setSpecies("Homo sapiens");
+    Human.setEnergy(9.5);
+    Human.setSize(165);
+    Human.setIsHungry(true);
+    Human.toString();
+    
+    Human.setLifeStatus(true);
+    Human.grow();
+    Human.setColor("A weird shade of tan");
+    Human.sleep();
+    Human.huntForFood();
+    Human.sleep();
+    Human.toString();
+  }
+  
+  public Human(isAlive, size, species, color, energy, isHungry) {
+    System.out.println(/t"Human Two: ");
+    
+    Human.setLifeStatus(isAlive);
+    Human.setSize(size);
+    Human.setSpecies(species);
+    Human.setColor(color);
+    Human.setEnergy(energy);
+    Human.setIsHungry(isHungry);
+    Human.toString();
+  }
