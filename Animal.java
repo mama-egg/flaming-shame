494fc0953c0338786590e666a6c4c470a95e9006
mama-egg <Kinseygoesrawr@gmail.com>
2014-12-14 21:27:36 -0700

    Update Animal.java

--- Animal.java
+++ Animal.java
@@ -2,5 +2,69 @@
 //9N
 
 public class Animal inherits Organism {
-
+  
+  super(isAlive, size, species);
+  private String color;
+  private double energy;
+  private boolean isHungry;
+  
+  private Animal() {
+    isAlive = false;
+    size = 0;
+    species = "";
+    color = "";
+    energy = 0.0;
+    isHungry = false;
+  }
+  
+  private Animal(boolean isAlive, int size, String species, String color, double energy, boolean isHungry) {
+    Animal.setLifeStatus(isAlive);
+    Animal.setSize(size);
+    Animal.setSpecies(species);
+    
+    Animal.setColor(color);
+    Animal.setEnergy(energy);
+    Animal.setIsHungry(isHungry);
+  }
+  
+  //Get Methods
+  private String getColor() {
+    return color;
+  }
+  
+  private String getEnergy() {
+    return energy;
+  }
+  
+  private boolean getIsHungry() {
+    return isHungry
+  }
+  
+  private String toString() {
+    this.toString();
+    return Organism.toString(species, isAlive, size);
+  }
+  
+  //Set Methods
+  private void setColor(String newColor) {
+    color = newColor;
+  }
+  
+  private void setEnergy(double newEnergy) {
+    energy = newEnergy;
+  }
+  
+  private void setIsHungry(boolean newHunger) {
+    isHungry = newHunger;
+  }
+  
+  //Other Mutators
+  private void huntForFood() {
+    energy--;
+    isHungry = false;
+  }
+  
+  private void sleep() {
+    energy += 2.5;
+  }
 }
